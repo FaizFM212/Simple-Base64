1b1f84b7bc7f3d3e736990d2e71e3ba1cea6f4bc
FaezFM212 <faizfm212@gmail.com>
2021-10-03 14:26:53 +0700

    It can save to the same file

--- Simple-Base64.py
+++ Simple-Base64.py
@@ -1,33 +1,43 @@
 import base64
+import time
 
 print("Created by: FaezFM212")
-print("Version: 1")
+print("Version: 1.1\n")
 
 #Choose and open an input file
 sourcePath = input("Open file: ")
 
 try:
-  FileInput = open(sourcePath, "rb")
+  File = open(sourcePath, "rb")
 except FileNotFoundError:
   print("File not found!")
+  time.sleep(1)
   exit()
 
 #Encode or Decode
 mode = input("Encode / Decode: ")
 
-#Writing to the output file
-savePath = input("Save as: ")
-FileOutput = open(savePath, "wb")
+#Processing
+base64String = ""
 
 if mode == "Encode":
-  FileOutput.write(base64.b64encode(FileInput.read()))
-  print("File writed successfully")
+  base64String = base64.b64encode(File.read())
 elif mode == "Decode":
-  FileOutput.write(base64.b64decode(FileInput.read()))
-  print("File writed successfully")
+  base64String = base64.b64decode(File.read())
 else:
   print("Please choose \"Encode\" or \"Decode\"")
+  time.sleep(1)
+  exit()
 
 #Close File
-FileInput.close()
-FileOutput.close()
+File.close()
+
+#Write to output file
+savePath = input("Save as: ")
+File = open(savePath, "wb")
+
+File.write(base64String)
+print("File writed successfully")
+
+#Close File
+File.close()
